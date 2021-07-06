02fed47b442330688259a3178a28a9da6e80c79f
mocanufelix <86775561+mocanufelix@users.noreply.github.com>
2021-07-06 12:14:08 +0300

    FM: First commit.

---
+++ src/main/java/com/demo/EContact/EContactApplication.java
@@ -0,0 +1,13 @@
+package com.demo.EContact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EContactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EContactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/EContact/EContactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.EContact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EContactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
