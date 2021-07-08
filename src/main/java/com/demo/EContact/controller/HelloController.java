2ed07bfbdd0021a83f1cbc2eb4e6a2da533ff7f4
mocanufelix <86775561+mocanufelix@users.noreply.github.com>
2021-07-08 10:32:34 +0300

    FM: First commit.

---
+++ src/main/java/com/demo/EContact/controller/HelloController.java
@@ -0,0 +1,30 @@
+package com.demo.EContact.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping (value = "/printHello")
+    @ResponseBody
+    public String printHello(){
+        return "Hello World!";
+    }
+
+    @GetMapping (value = "/printParam")
+    @ResponseBody
+    public String printParam(@RequestParam("username") String name){
+        return "Hello " + name;
+    }
+
+    @GetMapping (value = "/printPathVariable/{organization}")
+    @ResponseBody
+    public String printPathVariable(@PathVariable String organization){
+        return "Hello " + organization;
+    }
+
+}
