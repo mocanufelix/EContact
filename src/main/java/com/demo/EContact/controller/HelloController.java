d2bfcb7ea0b9d28d270f2e4d863d39f179c60ac6
mocanufelix <86775561+mocanufelix@users.noreply.github.com>
2021-07-09 11:02:32 +0300

    FM:Tabelul html cu bootstrap

--- src/main/java/com/demo/EContact/controller/HelloController.java
+++ src/main/java/com/demo/EContact/controller/HelloController.java
@@ -9,47 +9,27 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloController {
 
-<<<<<<< HEAD
     @GetMapping(value = "/printHello")
-=======
-    @GetMapping (value = "/printHello")
->>>>>>> FM: First commit.
     @ResponseBody
     public String printHello(){
         return "Hello World!";
     }
 
-<<<<<<< HEAD
-
     @GetMapping(value = "/printParam")
-=======
-    @GetMapping (value = "/printParam")
->>>>>>> FM: First commit.
     @ResponseBody
     public String printParam(@RequestParam("username") String name){
         return "Hello " + name;
     }
 
-<<<<<<< HEAD
-
-    @GetMapping (value = "/printPathVariable/{country}/index")
-    @ResponseBody
-    public String printPathVariable(@PathVariable String country){
-        return "Site country " + country  ;
-    }
-
-
-}
-
-
-
-
-=======
     @GetMapping (value = "/printPathVariable/{organization}")
     @ResponseBody
     public String printPathVariable(@PathVariable String organization){
         return "Hello " + organization;
     }
 
+    @GetMapping(value = "/printPathVariable/{country}/index")
+    @ResponseBody
+    public String printPathVariable(@PathVariable String country){
+        return "Site country " + country;
+    }
 }
->>>>>>> FM: First commit.
