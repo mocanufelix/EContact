2c5cf8194d1d5f5b024457186d209641a75f7878
mocanufelix <86775561+mocanufelix@users.noreply.github.com>
2021-07-19 15:13:22 +0300

    FM:comit7 Creere tabela default

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,10 @@
+ create table t_econtact(
+    id integer auto_increment,
+    lastName varchar(50),
+    firstName varchar(50),
+    phoneNumber varchar(50),
+    address varchar(50),
+    city varchar(50),
+    email varchar(50),
+    primary key (id)
+ );
