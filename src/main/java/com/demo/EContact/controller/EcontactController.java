e47b0948a77da9b28cfe2ebe37f36a33e46a4430
Daniel <tinteadanieldanny@yahoo.com>
2021-07-22 00:31:08 +0300

    update groups

--- src/main/java/com/demo/EContact/controller/EcontactController.java
+++ src/main/java/com/demo/EContact/controller/EcontactController.java
@@ -3,14 +3,13 @@ package com.demo.EContact.controller;
 
 
 import com.demo.EContact.entity.Econtact;
+import com.demo.EContact.entity.Group;
 import com.demo.EContact.service.EcontactService;
+import com.demo.EContact.service.GroupService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -20,6 +19,9 @@ public class EcontactController {
 
     @Autowired
     EcontactService econtactService;
+
+    @Autowired
+    GroupService groupService;
 
 
     @GetMapping(value="/EContact")
@@ -64,6 +66,9 @@ public class EcontactController {
         Econtact econtact = Econtact.builder().build();
         model.addAttribute("econtact", econtact);
 
+        List<Group> groupList = groupService.getGroup();
+        model.addAttribute("groupList", groupList);
+
         mav.setViewName("econtactForm");
         return mav;
     }
@@ -80,8 +85,9 @@ public class EcontactController {
     }
 
     @PostMapping (value = "/submitEcontact")
-    public ModelAndView submitEcontact(@ModelAttribute Econtact econtact){
+    public ModelAndView submitEcontact(@ModelAttribute Econtact econtact ){
         ModelAndView mav = new ModelAndView();
+
 
         econtactService.saveEcontactToDataBase(econtact);
 
@@ -90,13 +96,9 @@ public class EcontactController {
     }
 
     @PostMapping(value = "/editEcontact")
-    public ModelAndView editEcontact(@RequestParam("econtactId") int idEcontact, @RequestParam("econtactCity") String city, Model model){
+    public ModelAndView editEcontact( Model model){
 
         ModelAndView mav = new ModelAndView();
-
-        System.out.println("ID: "  + idEcontact);
-        System.out.println("city: "  + city);
-
         Econtact econtact = Econtact.builder()
                 .id(1)
                 .firstName("Felix")
@@ -109,6 +111,9 @@ public class EcontactController {
 
         model.addAttribute("econtact", econtact);
 
+
+        List<Group> groupList = groupService.getGroup();
+        model.addAttribute("groupList", groupList);
 
         mav.setViewName("econtactForm");
         return mav;
---
+++ src/main/java/com/demo/EContact/repository/GroupRepository.java
@@ -0,0 +1,9 @@
+package com.demo.EContact.repository;
+
+
+import com.demo.EContact.entity.Group;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GroupRepository extends JpaRepository<Group, Integer> {
+
+}
